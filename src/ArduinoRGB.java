78d4655fc2fbd772b7423df7d5387cb3dcd75f9f
emanuelecochi <lele.co@tiscali.it>
2015-12-28 16:52:32 +0100

    Port COM
    Added select port COM

--- src/ArduinoRGB.java
+++ src/ArduinoRGB.java
@@ -16,12 +16,18 @@ import java.util.Scanner;
 import java.awt.Font;
 import javax.swing.SwingConstants;
 import java.awt.Color;
+import javax.swing.JComboBox;
+import java.awt.Choice;
+import java.awt.event.ItemListener;
+import java.awt.event.ItemEvent;
 
 
 public class ArduinoRGB {
 
 	private JFrame frame;
 	SerialPort serialPort;
+	String[] listPort;
+	boolean init = false;
 
 	/**
 	 * Launch the application.
@@ -38,23 +44,15 @@ public class ArduinoRGB {
 			}
 		});
 	}
-
+	
 	public void serialInitialize() {
-		
-		// Salvo in listPort la lista delle porte seriale presenti
-    	String[] listPort = SerialJava.listPort();
-    	
-    	// Permetto la scelta della porta seriale da utilizzare
-    	Scanner s = new Scanner(System.in);
-		int chosenPort = s.nextInt();
-        serialPort = new SerialPort(listPort[chosenPort-1]);
-        
         try {
         	// Apro la porta seriale
             System.out.println("Port opened: " + serialPort.openPort());
             // Imposto i parametri della porta seriale 
             System.out.println("Params setted: " + serialPort.setParams(9600, 8, 1, 0));
             Thread.sleep(3000);
+            init = true;
         }
         catch (SerialPortException | InterruptedException ex){
             System.out.println(ex);
@@ -84,8 +82,8 @@ public class ArduinoRGB {
 	 * Create the application.
 	 */
 	public ArduinoRGB() {
-		serialInitialize();
 		initialize();
+		//serialInitialize();
 	}
 	
 	/**
@@ -94,7 +92,7 @@ public class ArduinoRGB {
 	private void initialize() {
 		int[] rgb = new int[3];
 		frame = new JFrame();
-		frame.setBounds(100, 100, 499, 238);
+		frame.setBounds(100, 100, 499, 311);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		JSlider sliderRed = new JSlider();
@@ -102,9 +100,11 @@ public class ArduinoRGB {
 		sliderRed.setMaximum(255);
 		sliderRed.addChangeListener(new ChangeListener() {
 			public void stateChanged(ChangeEvent arg0) {
+				if (init) {
 				rgb[0] = sliderRed.getValue();
 				serialWrite(rgb);
 				//System.out.println("RGB : " + rgb[0] + " " + rgb[1] + " " +rgb[2]);
+				}
 			}
 		});
 		
@@ -113,12 +113,15 @@ public class ArduinoRGB {
 		frame.getContentPane().add(sliderRed);
 		
 		JSlider sliderGreen = new JSlider();
+		sliderGreen.setEnabled(false);
 		sliderGreen.setMaximum(255);
 		sliderGreen.addChangeListener(new ChangeListener() {
 			public void stateChanged(ChangeEvent arg0) {
+				if (init) {
 				rgb[1] = sliderGreen.getValue();
 				serialWrite(rgb);
 				//System.out.println("RGB : " + rgb[0] + " " + rgb[1] + " " +rgb[2]);
+				}
 			}
 		});
 		sliderGreen.setValue(0);
@@ -126,12 +129,15 @@ public class ArduinoRGB {
 		frame.getContentPane().add(sliderGreen);
 		
 		JSlider sliderBlue = new JSlider();
+		sliderBlue.setEnabled(false);
 		sliderBlue.setMaximum(255);
 		sliderBlue.addChangeListener(new ChangeListener() {
 			public void stateChanged(ChangeEvent e) {
+				if (init) {
 				rgb[2] = sliderBlue.getValue();
 				serialWrite(rgb);
 				//System.out.println("RGB : " + rgb[0] + " " + rgb[1] + " " +rgb[2]);
+				}
 			}
 		});
 		sliderBlue.setValue(0);
@@ -141,7 +147,7 @@ public class ArduinoRGB {
 		JLabel lblNewLabel = new JLabel("Arduino RGB");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblNewLabel.setBounds(211, 24, 99, 23);
+		lblNewLabel.setBounds(204, 25, 99, 23);
 		frame.getContentPane().add(lblNewLabel);
 		
 		JLabel lblRed = new JLabel("RED");
@@ -164,6 +170,30 @@ public class ArduinoRGB {
 		lblBlue.setHorizontalAlignment(SwingConstants.CENTER);
 		lblBlue.setBounds(20, 150, 46, 14);
 		frame.getContentPane().add(lblBlue);
+		
+		listPort = SerialPortList.getPortNames();
+		Choice choice = new Choice();
+		choice.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent arg0) {
+				serialPort = new SerialPort(choice.getSelectedItem());
+				serialInitialize();
+				if (init) {
+					sliderRed.setEnabled(true);
+					sliderGreen.setEnabled(true);
+					sliderBlue.setEnabled(true);
+				}
+			}
+		});
+		choice.setBounds(233, 189, 107, 20);
+		for (String list: listPort)
+			choice.add(list);
+		frame.getContentPane().add(choice);
+		
+		JLabel lblPortCom = new JLabel("Select Port");
+		lblPortCom.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPortCom.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblPortCom.setBounds(150, 192, 77, 14);
+		frame.getContentPane().add(lblPortCom);
 		
 	}
 }
