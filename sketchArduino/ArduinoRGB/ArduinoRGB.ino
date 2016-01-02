e128995152df5290296c970da1f21fd991745358
emanuelecochi <lele.co@tiscali.it>
2016-01-02 16:37:39 +0100

    Used PIXEL_COUNT in for

--- sketchArduino/ArduinoRGB/ArduinoRGB.ino
+++ sketchArduino/ArduinoRGB/ArduinoRGB.ino
@@ -49,7 +49,7 @@ void loop() {
 
 // animation signle color
 void single_color(){
-  for (int i = 0; i < 20; i++)
+  for (int i = 0; i < PIXEL_COUNT ; i++)
     // strip.setPixelColor(n, red, green, blue);
     // The first argument — n in this example — is the pixel number along the strip, starting from 0 closest to the Arduino. 
     // If you have a strip of 30 pixels, they’re numbered 0 through 29.
@@ -63,7 +63,7 @@ void single_color(){
 void rainbow_light(int hue){
   // set the Moodlight values 
   ml.setHSB(hue, saturation , brightness);
-  for (int i = 0; i < 20; i++) 
+  for (int i = 0; i < PIXEL_COUNT ; i++) 
     strip.setPixelColor(i,ml.getRed(),ml.getGreen(),ml.getBlue());
   strip.show();
 }
